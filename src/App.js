707d50337fe098dd211635ddd029db47bcd951cc
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-13 23:41:08 +0200

    automatic and manual slides

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    } if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [people, index])
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000);
+    return () => clearInterval(slider)
+  }, [index])
+
   return <section className='section'>
     <div className="title">
       <h2>
@@ -32,10 +48,10 @@ function App() {
           </article>
         )
       })}
-      <button className="prev">
+      <button className="prev" onClick={() => setIndex(index - 1)}>
         <FiChevronLeft />
       </button>
-      <button className="next">
+      <button className="next" onClick={() => setIndex(index + 1)}>
         <FiChevronRight />
       </button>
     </div>
